fc643e83e33cec6b6c5bd16188a09d50e013e6d9
onethree-13 <onethrefred@gmail.com>
2019-02-19 20:13:10 -0800

    item pickup unifinished

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    Item item;
+    public Transform ItemParent;
+
+    public void ShowItem(Item newItem)
+    {
+        if (newItem != null)
+        {
+            item = newItem;
+            icon.sprite = item.icon;
+            icon.enabled = true;
+        }
+        else
+        {
+            item = null;
+            icon.sprite = null;
+            icon.enabled = false;
+        }
+    }
+}
