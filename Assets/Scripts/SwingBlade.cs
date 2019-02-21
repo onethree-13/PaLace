bd4a130f6acd6d92febed0019eff9b5d9e4c3c8e
Kumakawa <cym.christable@gmail.com>
2019-02-20 23:25:11 -0800

    created swing blad

---
+++ Assets/Scripts/SwingBlade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingBlade : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float leftPushRange;
+    public float rightPushRange;
+    public float velocityThreshold;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.angularVelocity = velocityThreshold;
+    }
+
+    private void Update()
+    {
+        Push();
+    }
+
+    private void Push()
+    {
+        if(transform.rotation.z > 0 && transform.position.z < rightPushRange && rigidbody2D.angularVelocity > 0 && rigidbody2D.angularVelocity < velocityThreshold)
+        {
+            rigidbody2D.angularVelocity = velocityThreshold;
+        }
+        else if(transform.rotation.z < 0 && transform.rotation.z > leftPushRange && rigidbody2D.angularVelocity < 0 && rigidbody2D.angularVelocity > velocityThreshold * -1)
+        {
+            rigidbody2D.angularVelocity = velocityThreshold * -1;
+        }
+    }
+}
