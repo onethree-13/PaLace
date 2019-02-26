4d121e0c2999ce29057d832f31dd0aa3f8c38eae
onethree-13 <onethreefred@gmail.com>
2019-02-26 14:09:28 -0800

    merge underground

---
+++ Assets/Scripts/WoodenSpikeTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodenSpikeTrigger : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    public float gravityScaleThreshold;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            rigidbody2D.gravityScale = gravityScaleThreshold;
+        }
+    }
+}
