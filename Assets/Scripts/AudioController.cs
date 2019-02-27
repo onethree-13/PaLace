7d955181de61409acf7642e1b4f1c45e11d277cc
Kumakawa <cym.christable@gmail.com>
2019-02-26 22:01:06 -0800

    add bgm

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public static AudioController audioController;
+
+    private void Awake()
+    {
+        if (audioController == null)
+        {
+            audioController = this;
+            DontDestroyOnLoad(audioController);
+        }
+        else if (audioController != this)
+        {
+            Destroy(audioController);
+        }
+    }
+}
