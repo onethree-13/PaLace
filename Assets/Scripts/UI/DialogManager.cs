c698a0c0642a4e7444f08cf104a819b92e7cad8e
onethree-13 <onethreefred@gmail.com>
2019-03-24 10:37:54 -0700

    DialogBox Partial

---
+++ Assets/Scripts/UI/DialogManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine.UI;
+using System.Collections ;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DialogManager : MonoBehaviour
+{
+    #region Singleton
+    public static DialogManager instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    private Queue<string> sentences;
+    public Text nameText;
+    public Text dialogText;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (Dialog dialog) {
+
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialog.name;
+        sentences.Clear();
+        foreach (string sentence in dialog.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialog()
+    {
+        animator.SetBool("IsOpen", false);
+        Debug.Log("End of Dialog");
+    }
+}
---
+++ Assets/Scripts/UI/DialogTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    public Dialog dialog;
+
+    public void TriggerDialog()
+    {
+        DialogManager.instance.StartDialogue(dialog);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            TriggerDialog();
+        }
+    }
+}
