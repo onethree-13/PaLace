330babe55ee220212219f03c3c43c2361bf06ed4
onethree-13 <onethreefred@gmail.com>
2019-04-01 19:32:20 -0700

    test

---
+++ Assets/CutSceneScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutSceneScript : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        timeline.Play();
+    }
+}
